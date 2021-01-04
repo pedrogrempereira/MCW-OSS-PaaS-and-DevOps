43473340178ce870264193fdf897a0c1e165dec2
Ubuntu <pedropereiramsft@outlook.com>
2021-01-04 12:55:39 +0000

    commit to master

--- Hands-on lab/lab-files/app.js
+++ Hands-on lab/lab-files/app.js
@@ -13,7 +13,7 @@ var userSession = require('./routes/session');
 
 var app = express();
 
-var databaseUrl = 'mongodb://localhost:27017/best-for-you-organics';
+var databaseUrl = 'mongodb://best-for-you-db-lab:dWAhppFEPkbNhmnRaAriPNYfYiULWYZlJVBWAwPoqDibqbboqsIriwbenW6eSiBmKekt5mg5DDOsvjm6KQ13LQ==@best-for-you-db-lab.mongo.cosmos.azure.com:10255/best-for-you-organics?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@best-for-you-db-lab@';
 
 var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
