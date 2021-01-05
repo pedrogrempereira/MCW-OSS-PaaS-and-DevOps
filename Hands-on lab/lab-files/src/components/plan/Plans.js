9554be894810d363a34548498dc14dfbd3cc41b1
Ubuntu <pedropereiramsft@outlook.com>
2021-01-05 09:45:34 +0000

    update

--- Hands-on lab/lab-files/src/components/plan/Plans.js
+++ Hands-on lab/lab-files/src/components/plan/Plans.js
@@ -58,7 +58,7 @@ export default class Plans extends Component {
 
         return (
             <div class="container">
-                <h3>Welcome to best of you Organics company cenas ah pois</h3>
+                <h3>Welcome to best of you Organics company cenas ah</h3>
                 <Grid>
                     <div class="plans">
                         <Row>{planCatalog}</Row>
